e59de0e7133db3b7ca1fd983ff30e503e843d172
Kacper Golinski <kacper.golinski@gmail.com>
2017-06-01 21:01:32 +0200

    rename env vars

--- src/reddit/submitPost.js
+++ src/reddit/submitPost.js
@@ -2,15 +2,15 @@ const snoowrap = require('snoowrap');
 
 require('dotenv').config();
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const API_SECRET = process.env.API_SECRET;
+const REDDIT_CLIENT_ID = process.env.REDDIT_CLIENT_ID;
+const REDDIT_API_SECRET = process.env.REDDIT_API_SECRET;
 const REDDIT_USERNAME = process.env.REDDIT_USERNAME;
 const REDDIT_PASSWORD = process.env.REDDIT_PASSWORD;
 
 const reddit = new snoowrap({
   userAgent: `Cool bot by /u/${REDDIT_USERNAME}`,
-  clientId: CLIENT_ID,
-  clientSecret: API_SECRET,
+  clientId: REDDIT_CLIENT_ID,
+  clientSecret: REDDIT_API_SECRET,
   username: REDDIT_USERNAME,
   password: REDDIT_PASSWORD
 });
