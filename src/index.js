b94819af09c2871b49e40362875ad9267079f281
Kacper Golinski <kacper.golinski@gmail.com>
2017-06-01 22:20:24 +0200

    updates

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// const submitPost = require('./reddit/submitPost');
+// const forBabelTest = require('./forBabelTest');
+
+exports.handler = (event, context, callback) => {
+  // submitPost();
+  // forBabelTest()
+    // .then((result) => {
+      // callback(null, result);
+    // });
+  callback(null, 'from directory');
+};
--- src/lambdaHandler.js
+++
@@ -1,7 +0,0 @@
-const submitPost = require('./reddit/submitPost');
-
-function lambdaHandler(/* event, context, callback */) {
-  submitPost();
-}
-
-module.exports = lambdaHandler;
