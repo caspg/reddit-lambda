9d7adac3e0ef84333729e1c51ce85f1dc0e8015c
Kacper Golinski <kacper.golinski@gmail.com>
2017-06-02 19:43:15 +0200

    clean ups

--- src/forBabelTest.js
+++
@@ -1,8 +0,0 @@
-const somePromise = () => new Promise((resolve) => resolve('resolved promise'));
-
-async function forBabelTest() {
-  const result = await somePromise();
-  return result;
-}
-
-module.exports = forBabelTest;
--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,12 +1,3 @@
-const forBabelTest = require('../src/forBabelTest');
-
 test('dummy example', () => {
   expect(true).toBe(true);
-});
-
-test('async await function', () => {
-  forBabelTest()
-    .then((result) => {
-      expect(result).toBe('resolved promise');
-    });
 });
